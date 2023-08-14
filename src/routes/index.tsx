b6c5aed99d50bd612250e3faef62e525ce212d54
JaydenLee1116 <vv55adss@gmail.com>
2023-08-14 15:16:59 +0900

    ♻️ refactor #4: Routes 컴포넌트 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, Routes, Route } from 'react-router-dom';
 
 import MainPage from '@pages/MainPage';
 import SignUpPage from '@pages/SignUpPage';
@@ -6,11 +6,12 @@ import SignInPage from '@pages/SignInPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <>
+    <Routes>
       <Route path="/" element={<MainPage />} />
       <Route path="signup" element={<SignUpPage />} />
       <Route path="signin" element={<SignInPage />} />
-    </>
+    </Routes>
   )
 );
+
 export default router;
